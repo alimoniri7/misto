9bb38042de87e64bb613a7d03daf1c442e7a6aaf
ali moniri <alimoniri81@gmail.com>
2023-05-03 22:27:47 +0430

    setup mui

---
+++ mui/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        tt: {
+            main: '#ac16a4'
+        }
+    }
+})
+
+export default theme
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
-import '@/styles/globals.css'
-import store from "../redux/store"
-import { Provider } from 'react-redux'
+import store from "../redux/store";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "@mui/material";
+import theme from "@/mui/theme";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider store={store} >
-      <Component {...pageProps} />
-    </Provider>
-  )
+    <ThemeProvider theme={theme} >
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </ThemeProvider>
+  );
 }
